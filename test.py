23ff96006c7cd447df20ebf164bd1be42abfef42
Jeevan145 <jeevant519@gmail.com>
2025-11-24 21:03:54 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import  matplotlib.pyplot as plt
+
+df = pd.read_csv("mtcars.csv")
+
+plt.hist(df['mpg'])
+plt.show()
+
+plt.scatter(df['wt'], df['mpg'])
+plt.show()
+
+df['am'].value_counts().plot(kind = 'bar')
+plt.show()
+
+plt.boxplot(df['mpg'])
+plt.show()
